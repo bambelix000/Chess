07156cc7ac0f1c544cff7fe4dd1f59aedb27bba1
Bartłomiej <bartekbaamb@gmail.com>
2023-04-02 17:27:02 +0200

    all pieces on board

--- src/game/Pieces.java
+++ src/game/Pieces.java
@@ -4,5 +4,7 @@ import javax.swing.*;
 import java.awt.image.BufferedImage;
 
 public class Pieces extends JPanel {
-    public BufferedImage whitePawn, blackPawn;
+    public BufferedImage whitePawn, blackPawn, whiteRook, blackRook,
+                        whiteKnight, blackKnight, whiteBishop, blackBishop,
+                        whiteQueen, blackQueen, whiteKing, blackKing;
 }
